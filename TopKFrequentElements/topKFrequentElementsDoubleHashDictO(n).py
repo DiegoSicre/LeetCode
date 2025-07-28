f7ad2428fefed82cbfb36964b78b195433cd3a7c
DiegoSicre <thedrakeishere12@gmail.com>
2025-07-28 23:43:47 +0200

    two Sum third round with onePass reasoning

--- TopKFrequentElements/topKFrequentElementsDoubleHashDictO(n).py
+++ TopKFrequentElements/topKFrequentElementsDoubleHashDictO(n).py
@@ -6,7 +6,7 @@ class Solution:
         """Given an integer array nums and an integer k, return the k most frequent elements. You may return 
         the answer in any order.
 
- 
+
 
         Example 1:
 
@@ -14,11 +14,12 @@ class Solution:
         Output: [1,2]
 
         Example 2:
+        
 
         Input: nums = [1], k = 1
         Output: [1]
 
-        
+
 
         Constraints:
 
@@ -27,10 +28,9 @@ class Solution:
             k is in the range [1, the number of unique elements in the array].
             It is guaranteed that the answer is unique.
         """
-        
-        
+
         #First, as 10-⁴ < nums [i] < 10⁴, I need to count them with a default dict
-        
+
         count_hash_dict : defaultdict[int, int] = defaultdict(int)
         maximum : int = 0
         for num in nums:
@@ -39,26 +39,25 @@ class Solution:
                 maximum = count_hash_dict[num]
         #Create an inverse dict that point from k(count) -> v(elements with that count)
         count_hash_dict_inverse : defaultdict[int, List[int]] = defaultdict(list[int])
-        
+
         for key,v in count_hash_dict.items():
+            
             count_hash_dict_inverse[v].append(key)
-        
-        
-        
+
         k_added_elements : int = 0
         i : int = maximum
         sol : List[int] = []
         #Return k elements O(k) worst case O(n)
         while(k_added_elements != k):
-            if(i in count_hash_dict_inverse):   
+            
+            if(i in count_hash_dict_inverse):
                 sol = sol + count_hash_dict_inverse[i]
                 k_added_elements = len(sol)
 
-               
+
             i-=1
         return sol
-        
+
         #Total time complexity O(3n)
-        
-        
+
 print(Solution().topKFrequent([1,1,1,2,2,3], 2))
