d9ab97474872d8a734a3a06dc46755048ccf73cc
DiegoSicre <thedrakeishere12@gmail.com>
2025-08-06 15:05:10 +0200

    Solved: EncodeAndDecode Strings for the secondTime, GroupAnagrams for the thirdTime and solved ProductsOfArrayExceptSelf in O(2n) time complexity and O(1) space complexity.

--- EncodeAndDecodeStrings/try.py
+++
@@ -1,9 +0,0 @@
-from base64 import encode
-
-
-character : str = "b"
-
-print(character.encode(encoding="utf-8"))
-
-
-letter_in_bytes : bytes = character.encode("utf-8", "ignore")
--- GroupAnagrams/thirdRun/groupAnagrams.py
+++ GroupAnagrams/thirdRun/groupAnagrams.py
@@ -1,4 +1,22 @@
+from collections import defaultdict
 from typing import List
 
-count : List[int] = [0] * 26
-
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+    
+        #In order to count the frequency of lower case english letters we can use an array of 26 cells
+        #cell with index 0 will represent 'a' and the other letters will be represented by the other index numbers in alphabetical order.
+        
+        #We are going to create a hashDict where they keys are the tupled arrays and the values a list of all the words having that count
+        hash_dict : defaultdict[tuple[int, ...], List[str]] = defaultdict(list[str])
+        for word in strs:
+            count :List[int] = [0] * 26
+            #As the max word length is 100 we can iterate the characters in 0(100)
+            for char in word:
+                count[ord(char) - ord("a")] += 1
+                
+            hash_dict[tuple(count)].append(word)
+            
+        return list(hash_dict.values())
+    
+print(Solution().groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
