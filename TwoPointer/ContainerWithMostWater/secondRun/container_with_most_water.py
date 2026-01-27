ce5ede4bf1debf8d283ebe2a12bf6284fabcbb49
diegosicre <thedrakeishere12@gmail.com>
2026-01-27 14:22:18 +0100

    Container with most water by moving the lower bound

---
+++ TwoPointer/ContainerWithMostWater/secondRun/container_with_most_water.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        i : int = 0
+        j : int = len(height) - 1
+        max_area : int = 0
+        while i < j:
+            max_area = max(max_area, min(height[i], height[j]) * (j - i))
+            
+            #The next index move will substract 1 unit of x length, therefore the maximum solution can only be achieved by moving the index of the pillar with less height
+            if height[i] <= height[j]:
+                i+=1
+            else:
+                j-=1
+        return max_area
